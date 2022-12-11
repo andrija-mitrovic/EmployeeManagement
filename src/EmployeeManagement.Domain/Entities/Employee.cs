035a473caf54fb68e03603d8021b9bde832463b0
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-11 10:24:19 +0100

    Add employee entity

---
+++ src/EmployeeManagement.Domain/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagement.Domain.Common;
+
+namespace EmployeeManagement.Domain.Entities
+{
+    public sealed class Employee : BaseEntity
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Title { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+    }
+}
