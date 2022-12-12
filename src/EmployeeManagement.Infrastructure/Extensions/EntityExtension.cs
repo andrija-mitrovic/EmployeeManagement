c32e0b232a91055a5d068910cd94856b590ada07
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-12 08:33:32 +0100

    Add entity extensions

---
+++ src/EmployeeManagement.Infrastructure/Extensions/EntityExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace Microsoft.EntityFrameworkCore.ChangeTracking
+{
+    internal static class EntityExtension
+    {
+        public static bool HasChangedOwnedEntities(this EntityEntry entry) =>
+            entry.References.Any(x =>
+                x.TargetEntry != null &&
+                x.TargetEntry.Metadata.IsOwned() &&
+                (x.TargetEntry.State == EntityState.Added || x.TargetEntry.State == EntityState.Modified));
+    }
+}
