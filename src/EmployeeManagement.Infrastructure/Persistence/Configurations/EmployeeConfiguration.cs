cb5a444288f9e4ad5a5485d189a3516c3f3973ad
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-12 08:33:51 +0100

    Add empoyee table configuration

---
+++ src/EmployeeManagement.Infrastructure/Persistence/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EmployeeManagement.Infrastructure.Persistence.Configurations
+{
+    internal class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(x => x.FirstName).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.LastName).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.Title).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.Email).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.Address).HasMaxLength(100).IsRequired();
+        }
+    }
+}
