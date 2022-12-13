7e115cd3344e802840be7d2f402024f2ac7b1436
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-13 08:30:17 +0100

    Add generic repository

---
+++ src/EmployeeManagement.Application/Common/Interfaces/IGenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+namespace EmployeeManagement.Application.Common.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IReadOnlyList<T>> GetAllAsync(bool disableTracking = true, CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate,
+                                        bool disableTracking = true,
+                                        CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>>? predicate,
+                                        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy,
+                                        string? includeString,
+                                        bool disableTracking = true,
+                                        CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<T?>> GetAsync(Expression<Func<T, bool>>? predicate,
+                                         Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy,
+                                         List<Expression<Func<T, object>>>? includes,
+                                         bool disableTracking = true,
+                                         CancellationToken cancellationToken = default);
+        Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<T> AddAsync(T entity, CancellationToken cancellationToken = default);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ src/EmployeeManagement.Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,85 @@
+﻿using EmployeeManagement.Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EmployeeManagement.Infrastructure.Persistence.Repositories
+{
+    internal class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _dbContext;
+
+        public GenericRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync(bool disableTracking = true, CancellationToken cancellationToken = default)
+        {
+            IQueryable<T> query = _dbContext.Set<T>();
+
+            if (disableTracking) query = query.AsNoTracking();
+
+            return await query.ToListAsync(cancellationToken);
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate, bool disableTracking = true, CancellationToken cancellationToken = default)
+        {
+            IQueryable<T> query = _dbContext.Set<T>();
+
+            if (disableTracking) query = query.AsNoTracking();
+            if (predicate != null) query = query.Where(predicate);
+
+            return await query.ToListAsync(cancellationToken);
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>>? predicate, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy, string? includeString, bool disableTracking = true, CancellationToken cancellationToken = default)
+        {
+            IQueryable<T> query = _dbContext.Set<T>();
+            if (disableTracking) query = query.AsNoTracking();
+
+            if (!string.IsNullOrWhiteSpace(includeString)) query = query.Include(includeString);
+
+            if (predicate != null) query = query.Where(predicate);
+
+            if (orderBy != null) return await orderBy(query).ToListAsync();
+
+            return await query.ToListAsync(cancellationToken);
+        }
+
+        public async Task<IReadOnlyList<T?>> GetAsync(Expression<Func<T, bool>>? predicate, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy, List<Expression<Func<T, object>>>? includes, bool disableTracking = true, CancellationToken cancellationToken = default)
+        {
+            IQueryable<T> query = _dbContext.Set<T>();
+
+            if (disableTracking) query = query.AsNoTracking();
+
+            if (includes != null) query = includes.Aggregate(query, (current, include) => current.Include(include));
+
+            if (predicate != null) query = query.Where(predicate);
+
+            if (orderBy != null) return await orderBy(query).ToListAsync();
+
+            return await query.ToListAsync(cancellationToken);
+        }
+
+        public async Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Set<T>().FindAsync(new object[] { id }, cancellationToken);
+        }
+
+        public async Task<T> AddAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            await _dbContext.Set<T>().AddAsync(entity, cancellationToken);
+            return entity;
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+        }
+    }
+}
