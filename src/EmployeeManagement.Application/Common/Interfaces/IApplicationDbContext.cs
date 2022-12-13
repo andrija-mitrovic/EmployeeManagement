b054f95ee8a353a15a057549292f57a1982d30c0
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-13 08:29:39 +0100

    Add application db context

---
+++ src/EmployeeManagement.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using EmployeeManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Employee> Employees { get; }
+    }
+}
---
+++ src/EmployeeManagement.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using EmployeeManagement.Application.Common.Interfaces;
+using EmployeeManagement.Domain.Entities;
+using EmployeeManagement.Infrastructure.Persistence.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace EmployeeManagement.Infrastructure.Persistence
+{
+    public sealed class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        private readonly AuditableEntitySaveChangesInterceptor _auditableEntitySaveChangesInterceptor;
+
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options,
+            AuditableEntitySaveChangesInterceptor auditableEntitySaveChangesInterceptor) : base(options)
+        {
+            _auditableEntitySaveChangesInterceptor = auditableEntitySaveChangesInterceptor;
+        }
+
+        public DbSet<Employee> Employees => Set<Employee>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.AddInterceptors(_auditableEntitySaveChangesInterceptor);
+        }
+    }
+}
