a58ea15f5b45f3ca685057e0f2c159bc29f10a03
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-13 08:30:39 +0100

    Add employee repository

---
+++ src/EmployeeManagement.Application/Common/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,8 @@
+﻿using EmployeeManagement.Domain.Entities;
+
+namespace EmployeeManagement.Application.Common.Interfaces
+{
+    public interface IEmployeeRepository : IGenericRepository<Employee>
+    {
+    }
+}
---
+++ src/EmployeeManagement.Infrastructure/Persistence/Repositories/EmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using EmployeeManagement.Application.Common.Interfaces;
+using EmployeeManagement.Domain.Entities;
+
+namespace EmployeeManagement.Infrastructure.Persistence.Repositories
+{
+    internal sealed class EmployeeRepository : GenericRepository<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(ApplicationDbContext dbContext) : base(dbContext) { }
+    }
+}
