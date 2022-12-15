1ebd74ab75415519441a4500378fc785d1c943ba
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-15 08:28:22 +0100

    Add http extension class

---
+++ src/EmployeeManagement.Application/Common/Extensions/HttpExtensions.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeManagement.Application.Common.Helpers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Microsoft.AspNetCore.Http
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalPages, totalItems);
+            var camelCaseFormatter = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader, camelCaseFormatter));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
+    }
+}
