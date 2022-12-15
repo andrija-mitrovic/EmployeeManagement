9eb8581866175811f6454663e7d13b0c847b56b7
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-15 08:26:56 +0100

    Add mapping class

---
+++ src/EmployeeManagement.Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EmployeeManagement.Application.Common.DTOs;
+using EmployeeManagement.Domain.Entities;
+
+namespace EmployeeManagement.Application.Common.Mappings
+{
+    public sealed class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Employee, EmployeeDto>().ReverseMap();
+        }
+    }
+}
