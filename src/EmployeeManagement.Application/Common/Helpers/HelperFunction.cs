3aefb952af14d3dfc618671bcf02f112875aae6b
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-15 08:27:15 +0100

    Add helper class

---
+++ src/EmployeeManagement.Application/Common/Helpers/HelperFunction.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace EmployeeManagement.Application.Common.Helpers
+{
+    public static class HelperFunction
+    {
+        public static string GetMethodName([CallerMemberName] string memberName = "")
+        {
+            return memberName;
+        }
+    }
+}
