d37c52f59a6917bff83a56d3fd56bcaa0c907f1c
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-18 08:36:56 +0100

    Get employees query handler

---
+++ src/EmployeeManagement.Application/Employees/Queries/GetEmployees/GetEmployeesQuery.cs
@@ -0,0 +1,9 @@
+﻿using EmployeeManagement.Application.Common.DTOs;
+using MediatR;
+
+namespace EmployeeManagement.Application.Employees.Queries.GetEmployees
+{
+    public sealed class GetEmployeesQuery : IRequest<List<EmployeeDto>>
+    {
+    }
+}
---
+++ src/EmployeeManagement.Application/Employees/Queries/GetEmployees/GetEmployeesQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using EmployeeManagement.Application.Common.DTOs;
+using EmployeeManagement.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Application.Employees.Queries.GetEmployees
+{
+    internal sealed class GetEmployeesQueryHandler : IRequestHandler<GetEmployeesQuery, List<EmployeeDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetEmployeesQueryHandler(
+            IApplicationDbContext context,
+            IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<List<EmployeeDto>> Handle(GetEmployeesQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _context.Employees.AsNoTracking().ToListAsync(cancellationToken);
+
+            return _mapper.Map<List<EmployeeDto>>(products);
+        }
+    }
+}
