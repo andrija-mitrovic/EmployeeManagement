82b8cd86665266acecbb54f11eae1419ab661356
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-18 08:36:32 +0100

    Get employee by id query handler

---
+++ src/EmployeeManagement.Application/Employees/Queries/GetEmployeeById/GetEmployeeByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using EmployeeManagement.Application.Common.DTOs;
+using MediatR;
+
+namespace EmployeeManagement.Application.Employees.Queries.GetEmployeeById
+{
+    public sealed class GetEmployeeByIdQuery : IRequest<EmployeeDto>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/EmployeeManagement.Application/Employees/Queries/GetEmployeeById/GetEmployeeByIdQueryHandler.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using EmployeeManagement.Application.Common.DTOs;
+using EmployeeManagement.Application.Common.Exceptions;
+using EmployeeManagement.Application.Common.Interfaces;
+using EmployeeManagement.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Application.Employees.Queries.GetEmployeeById
+{
+    internal sealed class GetEmployeeByIdQueryHandler : IRequestHandler<GetEmployeeByIdQuery, EmployeeDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly ILogger<GetEmployeeByIdQueryHandler> _logger;
+
+        public GetEmployeeByIdQueryHandler(
+            IApplicationDbContext context,
+            IMapper mapper,
+            ILogger<GetEmployeeByIdQueryHandler> logger)
+        {
+            _context = context;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<EmployeeDto> Handle(GetEmployeeByIdQuery request, CancellationToken cancellationToken)
+        {
+            var employee = await _context.Employees.AsNoTracking().FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+            if (employee == null)
+            {
+                _logger.LogError(nameof(Employee) + " with Id: {EmployeeId} was not found.", request.Id);
+                throw new NotFoundException(nameof(Employee), request.Id);
+            }
+
+            return _mapper.Map<EmployeeDto>(employee);
+        }
+    }
+}
