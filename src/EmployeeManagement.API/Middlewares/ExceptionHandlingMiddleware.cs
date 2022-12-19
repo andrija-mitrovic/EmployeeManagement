3baa6caa02a0696816ea379dcd5dcb38108bcc25
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-19 08:22:18 +0100

    Add exception handling middleware

---
+++ src/EmployeeManagement.API/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using EmployeeManagement.Application.Common.Exceptions;
+using System.Text.Json;
+
+namespace EmployeeManagement.API.Middlewares
+{
+    internal sealed class ExceptionHandlingMiddleware : IMiddleware
+    {
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong: {ex}");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+
+            context.Response.StatusCode = exception switch
+            {
+                ValidationException => StatusCodes.Status400BadRequest,
+                ForbiddenAccessException => StatusCodes.Status403Forbidden,
+                NotFoundException => StatusCodes.Status404NotFound,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            var response = new
+            {
+                statusCode = context.Response.StatusCode,
+                error = exception.Message
+            };
+
+            await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+    }
+}
