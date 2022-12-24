bf0b915747da68e498af21b4ae0dcc48e8d31e56
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-24 09:14:13 +0100

    Add and configure EmployeeManagement client project

---
+++ src/EmployeeManagement.Client/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using EmployeeManagement.Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace EmployeeManagement.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Employees()
+        {
+            var httpClient = _httpClientFactory.CreateClient("APIClient");
+
+            var response = await httpClient.GetAsync("api/employees").ConfigureAwait(false);
+
+            response.EnsureSuccessStatusCode();
+
+            var employeesString = await response.Content.ReadAsStringAsync();
+            var employees = JsonSerializer.Deserialize<List<EmployeeViewModel>>(employeesString, 
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            return View(employees);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/EmployeeManagement.Client/Models/EmployeeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeManagement.Client.Models
+{
+    public class EmployeeViewModel
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+    }
+}
