2e16cff7155d4f55c7103f7f68c7e0fd9ebba371
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-27 08:16:41 +0100

    Add HelperFunction test

---
+++ tests/EmployeeManagement.Application.UnitTests/Common/Helpers/HelperFunctionTest.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeManagement.Application.Common.Helpers;
+using FluentAssertions;
+
+namespace EmployeeManagement.Application.UnitTests.Common.Helpers
+{
+    public sealed class HelperFunctionTest
+    {
+        [Theory]
+        [InlineData("CreateEmployee")]
+        public void GetMethodName_ShouldGetMethodName_WhenArgumentIsPassed(string input)
+        {
+            var result = HelperFunction.GetMethodName(input);
+
+            result.Should().BeEquivalentTo(input);
+        }
+    }
+}
