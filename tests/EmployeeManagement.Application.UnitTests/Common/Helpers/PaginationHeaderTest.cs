f7410be6666d574e5f0bcd71a3c04f8402cd47d4
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-28 08:21:06 +0100

    Add test for PaginationHeader class

---
+++ tests/EmployeeManagement.Application.UnitTests/Common/Helpers/PaginationHeaderTest.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagement.Application.Common.Helpers;
+using FluentAssertions;
+
+namespace EmployeeManagement.Application.UnitTests.Common.Helpers
+{
+    public sealed class PaginationHeaderTest
+    {
+        [Fact]
+        public void DefaultConstructorCreatesPaginationHeaderInstance()
+        {
+            var result = new PaginationHeader(1, 5, 3, 15);
+
+            result.CurrentPage.Should().Be(1);
+            result.PageSize.Should().Be(5);
+            result.TotalPages.Should().Be(3);
+            result.TotalCount.Should().Be(15);
+        }
+    }
+}
