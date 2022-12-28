06ede4d388b5f3e2d8aac25035b861d8fd324cf7
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-28 08:20:44 +0100

    Add test for PaginatedList

---
+++ tests/EmployeeManagement.Application.UnitTests/Common/Helpers/PaginatedListTest.cs
@@ -0,0 +1,60 @@
+﻿using EmployeeManagement.Application.Common.Helpers;
+using EmployeeManagement.Domain.Entities;
+using FluentAssertions;
+
+namespace EmployeeManagement.Application.UnitTests.Common.Helpers
+{
+    public sealed class PaginatedListTest
+    {
+        [Fact]
+        public void DefaultConstructorCreatesPaginatedListInstance()
+        {
+            List<Employee> employees = ReturnEmployees();
+
+            var result = new PaginatedList<Employee>(employees, 4, 1, 2);
+
+            result.PageNumber.Should().Be(1);
+            result.TotalPages.Should().Be(2);
+            result.TotalCount.Should().Be(4);
+        }
+
+        private static List<Employee> ReturnEmployees()
+        {
+            return new List<Employee>()
+            {
+                new Employee()
+                {
+                    FirstName = "Andrija",
+                    LastName = "Mitrovic",
+                    Title = "Software engineer",
+                    Address = "Herceg Novi",
+                    Email = "andrija@gmail.com"
+                },
+                new Employee()
+                {
+                    FirstName = "Marko",
+                    LastName = "Markovic",
+                    Title = "Project manager",
+                    Address = "Podgorica",
+                    Email = "marko@gmail.com"
+                },
+                new Employee()
+                {
+                    FirstName = "Ivan",
+                    LastName = "Ivanovic",
+                    Title = "Software engineer",
+                    Address = "Kotor",
+                    Email = "ivan@gmail.com"
+                },
+                new Employee()
+                {
+                    FirstName = "Petar",
+                    LastName = "Petrovic",
+                    Title = "Project manager",
+                    Address = "Niksic",
+                    Email = "petar@gmail.com"
+                }
+            };
+        }
+    }
+}
