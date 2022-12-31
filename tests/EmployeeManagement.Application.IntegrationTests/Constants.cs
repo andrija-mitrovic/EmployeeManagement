d39fe936b2bd2a5fc7f1bf66421304e479360483
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-31 08:49:29 +0100

    Add class for storing constants

---
+++ tests/EmployeeManagement.Application.IntegrationTests/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeManagement.Application.IntegrationTests
+{
+    internal static class Constants
+    {
+        public const string APPSETTINGS_FILE = "appsettings.json";
+        public const string CONNECTION_STRING_NAME = "SqlServerConnection";
+    }
+}
