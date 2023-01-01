2fb4e7327e4aa87808288efdaa5618c2b9d54871
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-01-01 10:54:02 +0100

    Configure IdenitityServer to use Authorization Code flow

--- src/EmployeeManagement.Identity/Config.cs
+++ src/EmployeeManagement.Identity/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -26,6 +27,25 @@ namespace EmployeeManagement.Identity
 
         public static IEnumerable<Client> Clients =>
             new Client[]
-            { };
+            { 
+                new Client
+                {
+                    ClientName = "EmployeeManagementClient",
+                    ClientId = "employeemanagementclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = new List<string> { "https://localhost:5010/signin-oidc" },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile, 
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha512())
+                    },
+                    RequirePkce = false,
+                    RequireConsent = true
+                }
+            };
     }
 }
