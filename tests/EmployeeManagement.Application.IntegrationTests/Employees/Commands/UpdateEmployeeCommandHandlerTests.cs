f8a08f60da81621c5beefaa361eebb3c9757fe86
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-01-01 10:53:11 +0100

    Add UpdateEmployeeCommandHandler integration test

---
+++ tests/EmployeeManagement.Application.IntegrationTests/Employees/Commands/UpdateEmployeeCommandHandlerTests.cs
@@ -0,0 +1,98 @@
+﻿using AutoMapper;
+using EmployeeManagement.API.Services;
+using EmployeeManagement.Application.Common.Interfaces;
+using EmployeeManagement.Application.Employees.Commands.UpdateEmployee;
+using EmployeeManagement.Infrastructure.Persistence.Interceptors;
+using EmployeeManagement.Infrastructure.Persistence.Repositories;
+using EmployeeManagement.Infrastructure.Persistence;
+using EmployeeManagement.Infrastructure.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using EmployeeManagement.Application.Common.Mappings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using FluentAssertions;
+using MediatR;
+
+namespace EmployeeManagement.Application.IntegrationTests.Employees.Commands
+{
+    public sealed class UpdateEmployeeCommandHandlerTests
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UpdateEmployeeCommandHandler> _logger;
+
+        public UpdateEmployeeCommandHandlerTests()
+        {
+            var configuration = ReturnConfiguration();
+            var optionsBuilder = ReturnDbOptionsBuilder(configuration);
+            var currentUserService = new CurrentUserService(new HttpContextAccessor());
+            var interceptor = new AuditableEntitySaveChangesInterceptor(currentUserService, new DateTimeService());
+            var applicationDbContext = new ApplicationDbContext(optionsBuilder.Options, interceptor);
+            var mapperConfiguration = ReturnMapperConfiguration();
+            var serviceProvider = ReturnServiceProvider();
+            var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+
+            _employeeRepository = new EmployeeRepository(applicationDbContext);
+            _unitOfWork = new UnitOfWork(applicationDbContext);
+            _mapper = mapperConfiguration.CreateMapper();
+            _logger = loggerFactory!.CreateLogger<UpdateEmployeeCommandHandler>();
+        }
+
+        private static IConfigurationRoot ReturnConfiguration()
+        {
+            return new ConfigurationBuilder().AddJsonFile(Constants.APPSETTINGS_FILE, true, true).Build();
+        }
+
+        private static DbContextOptionsBuilder<ApplicationDbContext> ReturnDbOptionsBuilder(IConfigurationRoot configuration)
+        {
+            return new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlServer(configuration.GetConnectionString(Constants.CONNECTION_STRING_NAME));
+        }
+
+        private static MapperConfiguration ReturnMapperConfiguration()
+        {
+            return new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+        }
+
+        private static ServiceProvider ReturnServiceProvider()
+        {
+            return new ServiceCollection().AddLogging().BuildServiceProvider();
+        }
+
+        [Fact]
+        public async Task Handle_ShouldUpdateEmployee_WhenUpdateRequestIsValid()
+        {
+            var command = ReturnUpdateEmployeeCommand();
+
+            var handler = new UpdateEmployeeCommandHandler(
+                _employeeRepository,
+                _unitOfWork,
+                _mapper,
+                _logger);
+
+            var result = await handler.Handle(command, default);
+
+            result.Should().NotBeNull();
+            result.Should().Be(Unit.Value);
+        }
+
+        private static UpdateEmployeeCommand ReturnUpdateEmployeeCommand()
+        {
+            return new()
+            {
+                Id = 2,
+                FirstName = "Andrija",
+                LastName = "Mitrovic",
+                Title = "Software engineer",
+                Address = "Herceg Novi",
+                Email = "andrija@gmail.com"
+            };
+        }
+    }
+}
